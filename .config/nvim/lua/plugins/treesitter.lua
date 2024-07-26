c3b63e7e8a7586ced84c9c67e6ceabad429caf63
Timo Gerstenhauer <timo.gerstenhauer@gmx.de>
2024-07-26 13:58:18 +0000

    added lsps

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
 	'nvim-treesitter/nvim-treesitter',
 	opts = {
-		ensure_installed= {'lua', 'html', 'css', 'javascript', 'typescript', 'markdown'},
+		ensure_installed= {'lua', 'html', 'css', 'javascript', 'typescript', 'markdown', 'python'},
 		auto_install = true,
 		highlight = {
 			enbale = true
